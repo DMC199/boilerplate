58a02ede6b6dde1ebd97d6d5fd7105196f5473b6
Ben Purdy <bpurdy@addc.oregonstoryboard.org>
2016-05-19 21:03:46 -0700

    Adding interactible script to apply material when gaze cursor hits an object

---
+++ Assets/Scripts/CursorHighlight.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using HoloToolkit.Unity;
+
+public class CursorHighlight : MonoBehaviour {
+
+    public GameObject FocusedGameObject { get; private set; }
+    public GameObject oldFocusedGameObject = null;
+
+	// Use this for initialization
+	void Start () {
+        FocusedGameObject = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        oldFocusedGameObject = FocusedGameObject;
+
+        if (GazeManager.Instance.Hit)
+        {
+            RaycastHit hitInfo = GazeManager.Instance.HitInfo;
+            if (hitInfo.collider != null)
+            {
+                // Assign the hitInfo's collider gameObject to the FocusedGameObject.
+                FocusedGameObject = hitInfo.collider.gameObject;
+            }
+            else
+            {
+                FocusedGameObject = null;
+            }
+        }
+        else
+        {
+            FocusedGameObject = null;
+        }
+
+        if (FocusedGameObject != oldFocusedGameObject)
+        {
+            ResetFocusedInteractible();
+
+            if (FocusedGameObject != null)
+            {
+                if (FocusedGameObject.GetComponent<Interactible>() != null)
+                {
+                    // Send a GazeEntered message to the FocusedGameObject.
+                    FocusedGameObject.SendMessage("GazeEntered");
+                }
+            }
+        }
+    }
+
+    private void ResetFocusedInteractible()
+    {
+        if (oldFocusedGameObject != null)
+        {
+            if (oldFocusedGameObject.GetComponent<Interactible>() != null)
+            {
+                // Send a GazeExited message to the oldFocusedGameObject.
+                oldFocusedGameObject.SendMessage("GazeExited");
+            }
+        }
+    }
+}
